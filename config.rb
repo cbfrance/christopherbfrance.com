e2f24304d4ca7bf045e13acd2b460092611e39cb
Chris Blow <chris@meedan.com>
2016-05-18 14:50:56 -0700

    Fix some image paths, remove feed.xml

--- config.rb
+++ config.rb
@@ -45,7 +45,7 @@ activate :blog do |blog|
   # blog.page_link = "page/{num}"
 end
 
-page "/feed.xml", layout: false
+# page "/feed.xml", layout: false
 # Reload the browser automatically whenever files change
 # configure :development do
 #   activate :livereload
