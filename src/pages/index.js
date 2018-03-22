41e52ab65ba1c502ea7111bc46caba0d3095b847
Chris Blow <chris@meedan.com>
2018-03-21 23:41:30 -0700

    Install styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  border: 1px solid aqua;
+`;
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
+    <Title>Hi people</Title>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
