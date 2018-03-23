8fcfc346608de07d914086dd7fe0fe3f900344ae
Chris Blow <chris@meedan.com>
2018-03-22 20:43:49 -0700

    Fix some errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,23 +12,25 @@ const yellow = "#ffdc46";
 const black = "#101312";
 
 const Title = styled.h1`
-  font-size: 30px;
-  padding-top: 16px;
+  font-size: 32px;
+  padding-top: 80px;
+  padding-bottom: 80px;
 `;
 
 const Container = styled.div`
-  width: 800px;
-  height: 1246px;
+  width: 762px;
+  height: 1203px;
+  position relative;
 `;
 
 const Reference = styled.div`
-  background: url("./static/reference/mondrian.jpg") no-repeat;
+  background: url("./static/reference/mondrian.jpg");
+  background-repeat: no-repeat;
   height: 100%;
   width: 100%;
   position: absolute;
-  width: 800px;
-  height: 1246px;
   opacity: 0.5;
+  border: 2px solid aqua;
 `;
 
 const Block = styled.div`
@@ -41,57 +43,55 @@ const Block = styled.div`
 const P = styled.p`
   font-family: system-ui, -apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
   padding-top: ${props => props.padTop ? "80px" : null};
+  margin: 0;
 `;
 
 const ParentGrid = styled.div`
   display: grid;
-  grid-template-columns: 72px 140px 410px 118px 22px 36px;
-  grid-template-rows: 96px 394px 218px 60px 202px 232px 44px;
-  grid-template-areas: "A1 A1 A2 A2 A2"
-                       "B1 B1 A2 A2 A2"
+  grid-template-columns: 72px 140px 410px 118px 22px;
+  grid-template-rows: 96px 394px 218px 60px 202px 232px;
+  grid-template-areas: "A1 A1 A2 A2 A3"
+                       "B1 B1 A2 A2 A3"
                        "B1 B1 C1 C1 C2"
                        "D1 D2 D3 D4 C2"
                        "E1 E2 D3 D4 C2"
-                       "E1 E2 F1 F2 F2"
-                       "G1 G1 G2 G2 G2";
+                       "E1 E2 F1 F2 F2";
   height: 100%;
   width: 100%;
+
+  &, & > * {
+    box-sizing: border-box;
+  }
 `;
 
 const IndexPage = () => (
   <div>
     <Title>Composition with Large Blue Plane, Red, Black, Yellow, and Gray (1921) by Piet Mondrian</Title>
     <Container>
-      <Reference/>
       <ParentGrid>
-        <Block area="A1" borderRight color={black} />
-        <Block area="A2" color={red} />
-        <Block area="A3" color={black} />
+        <Block area="A1" color={black} borderRight />
+        <Block area="A2" color={red} borderBottom />
+        <Block area="A3" color={red} borderBottom />
         <Block area="B1" color={grey} borderRight borderBottom />
-        <Block area="B2" color={black} />
-        <Block area="B2" color={grey} />
         <Block area="C1" color={grey} borderRight borderBottom />
         <Block area="C2" color={grey} borderBottom />
-        <Block area="C3" color={grey} borderBottom />
         <Block area="D1" color={black} borderRight />
         <Block area="D2" color={grey} borderRight />
         <Block area="D3" color={grey} borderBottom />
         <Block area="D4" color={grey} borderRight borderBottom />
-        <Block area="D5" color={black} />
         <Block area="E1" color={yellow} borderRight />
         <Block area="E2" color={grey} borderRight />
-        <Block area="E3" color={yellow} />
         <Block area="F1" color={grey} borderRight borderBottom />
         <Block area="F2" color={blue} borderBottom />
-        <Block area="G1" color={black} />
-        <Block area="G2" color={red} />
-        <Block area="G3" color={black} />
       </ParentGrid>
     </Container>
 
-    <P padTop>Pieter Cornelis "Piet" Mondriaan, after 1906 Mondrian (/ˈmɔːndriˌɑːn, ˈmɒn-/; Dutch: [ˈpit ˈmɔndrijaːn], later [ˈmɔndrijɑn]; 7 March 1872 – 1 February 1944), was a Dutch painter. Mondrian was a contributor to the De Stijl art movement and group, which was founded by Theo van Doesburg. He evolved a non-representational form which he termed neoplasticism. This consisted of white ground, upon which he painted a grid of vertical and horizontal black lines and the three primary colors.Mondrian's arrival in Paris from the Netherlands in 1911 marked the beginning of a period of profound change. He encountered experiments in Cubism and with the intent of integrating himself within the Parisian avant-garde removed an 'a' from the Dutch spelling of his name (Mondriaan).</P>
 
-    <Link to="/page-2/">Go to page 2</Link>
+    <P padTop>“Art is higher than reality and has no direct relation to reality. To approach the spiritual in art, one will make as little use as possible of reality, because reality is opposed to the spiritual. We find ourselves in the presence of an abstract art. Art should be above reality, otherwise it would have no value for [humans].”</P>
+
+    <P padTop>Pieter Cornelis "Piet" Mondriaan, after 1906 Mondrian (/ˈmɔːndriˌɑːn, ˈmɒn-/; Dutch: [ˈpit ˈmɔndrijaːn], later [ˈmɔndrijɑn]; 7 March 1872 – 1 February 1944), was a Dutch painter. Mondrian was a contributor to the De Stijl art movement and group, which was founded by Theo van Doesburg. He evolved a non-representational form which he termed neoplasticism. This consisted of white ground, upon which he painted a grid of vertical and horizontal black lines and the three primary colors. Mondrian's arrival in Paris from the Netherlands in 1911 marked the beginning of a period of profound change. He encountered experiments in Cubism and with the intent of integrating himself within the Parisian avant-garde removed an 'a' from the Dutch spelling of his name (Mondriaan).</P>
+
+      <P padTop>— <a href="https://en.wikipedia.org/wiki/Piet_Mondrian">Wikipedia</a></P>
   </div>
 )
 
