13be4a872279f84ac1a2ebc219581983c318a2d7
Chris Blow <chris@meedan.com>
2018-04-23 23:40:11 -0700

    Image path migration script

--- fix_images_migration.sh
+++ fix_images_migration.sh
@@ -6,5 +6,5 @@ for filepath in src/posts/[0-9]*/**/*.md; do
   echo $filepath
 
   # Fix the image paths
-  gsed -E -i 's/src=\/images/src=\/static/images/g' $filepath
+  gsed -E -i 's/src="\/images/src="\/static\/images/g' $filepath
 done
