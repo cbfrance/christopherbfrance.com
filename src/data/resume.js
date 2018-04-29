98f004fb0223e7982748494b3990732a3084c7cd
Chris Blow <chris@meedan.com>
2018-04-29 13:18:10 -0700

    Parse resume data as JSON

---
+++ src/data/resume.js
@@ -0,0 +1,185 @@
+export const resumeData = `{
+  "basics": {
+    "name": "Chris Gerald Blow",
+    "label": "Programmer",
+    "picture": "none",
+    "email": "cgblow@gmail.com",
+    "phone": "(415) 309-7900",
+    "website": "https://unthinkingly.com",
+    "summary":
+      "Hello, I’m Chris, a designer living in the San Francisco Bay Area. I work for Meedan, where I’ve been practicing design strategy for 10 years. I have experience in product design, user experience research, front-end programming and investigative journalism. My work typically involves themes of collaborative work and cross-language communication. I am especially interested in user research for the advancement improvement of journalism and civil society.",
+    "location": {
+      "address": "none",
+      "postalCode": "CA 94611",
+      "city": "Oakland",
+      "countryCode": "USA",
+      "region": "California"
+    },
+    "profiles": [
+      {
+        "network": "Twitter",
+        "username": "unthinkingly",
+        "url": "https://twitter.com/unthinkingly"
+      },
+      {
+        "network": "Github",
+        "username": "chrisblow",
+        "url": "https://github.com/chrisblow"
+      }
+    ]
+  },
+  "work": [
+    {
+      "company": "Meedan",
+      "position": "Design Director",
+      "website": "http://meedan.com",
+      "startDate": "March 2016",
+      "endDate": "Current",
+      "summary": "I created graphic design, front-end code, design systems and strategies to support a product team creating new media projects.",
+      "highlights": ["Launched award-winning product", "Managed designers and design process", "Developed style guides, defined best practices"]
+    },
+    {
+      "company": "Meedan",
+      "position": "Director of User Experience",
+      "website": "http://meedan.com",
+      "startDate": "March 2008",
+      "endDate": "March 2016",
+      "summary": "I developed user experience research methods appropriate to a nimble product development process.",
+      "highlights": [
+        "Managed a lab user research project for a large Facebook project",
+        "Managed a user research process for Cambridge University and other clients",
+        "Developed user remote research process"
+      ]
+    },
+    {
+      "company": "Bolt | Peters User Experience",
+      "position": "Researcher, Developer & Designer",
+      "website": "https://www.crunchbase.com/organization/bolt-peters#section-overview",
+      "startDate": "July 2007",
+      "endDate": "September 2008",
+      "summary":
+        "As a User Experience Researcher was responsible for designing studies, analyzing results, collaborating with clients. I created videos, wireframes and sketches. I also helped design and build Ethnio, a Rails app for the user experience industry.",
+      "highlights": [
+        "Helped build Ethnio as a Rails developer",
+        "Managed research for clients such as Sony and Harvard Business Review",
+        "Expanded the company services to include design practice"
+      ]
+    },
+    {
+      "company": "Blast Internet Services and Web Design",
+      "position": "Director of Operations and Sales",
+      "website": "none",
+      "startDate": "March 2006",
+      "endDate": "July 2007",
+      "summary":
+        "I revitalized their web consulting business with user-centric voice. I designed contracts and won budgets to implement an Agile development methodology and the Rails stack.",
+      "highlights": [
+        "Quadrupled sales in 6 months by winning clients such as Duke university and Burt's Bees",
+        "Reduced email spam by 90% by implemented greylisting",
+        "Managed successful development team transition from Perl to Ruby"
+      ]
+    },
+    {
+      "company": "Evaluation, Assessment and Policy Connections (EvAP)",
+      "position": "Web Developer",
+      "website": "http://www.unc.edu/depts/ed/evap/",
+      "startDate": "May 2004",
+      "endDate": "March 2006",
+      "summary": "At EvAP I developed applications for managing stakeholder data, creating educational surveys and gathering real-time feedback.",
+      "highlights": [
+        "Develped PHP application for generating RTF surveys",
+        "Developed web-based feedback tool for evaluation process",
+        "Developed Microsoft Access database for central storage of stakeholder data"
+      ]
+    },
+    {
+      "company": "Nonprofit Design",
+      "position": "Web Developer",
+      "website": "none",
+      "startDate": "May 2003",
+      "endDate": "March 2006",
+      "summary": "I operated a consultancy with clients such as Duke University and the University of North Carolina at Chapel Hil.",
+      "highlights": [
+        "Designed and developed print material",
+        "Created logos and branding campaigns",
+        "Created interactive CDs for educational department"
+      ]
+    },
+    {
+      "company": "The North Carolina Center on Actual Innocence, Duke and UNC-Chapel Hill",
+      "position": "Teaching Assistant",
+      "website": "none",
+      "startDate": "August 2003",
+      "endDate": "December 2003",
+      "summary":
+        "I was a teaching assistant for investigative journalism course at the UNC-CH School of Journalism, in partnership with Duke University.",
+      "highlights": ["Helped students with investigative research", "Led an investigation into a 20-year-old cold case"]
+    },
+    {
+      "company": "The North Carolina Law Review",
+      "position": "Web developer",
+      "website": "http://scholarship.law.unc.edu/nclr/",
+      "startDate": "August 2001",
+      "endDate": "December 2003",
+      "summary": "I helped create a custom database for the Law Review, and I managed their website.",
+      "highlights": [" ", " "]
+    }
+  ],
+  "volunteer": [
+    {
+      "organization": "Ushahidi",
+      "position": "Interaction Designer",
+      "website": "http://ushahidi.com/",
+      "startDate": "September 2008",
+      "endDate": "April 2010",
+      "summary": "Worked with Ushiaidi, a crowdsourcing platform for crises.",
+      "highlights": [
+        "Helped design the initial Ushahidi iPhone app",
+        "Helped designed multiple hosted instances concept, later fundedd as crowdmap.com",
+        "Design Swift, later funded as a multi-year project"
+      ]
+    }
+  ],
+  "education": [
+    {
+      "institution": "University of North Carolina at Chapel Hill",
+      "area": "Mass Communications",
+      "studyType": "Bachelor of Arts",
+      "startDate": "2011-06-01",
+      "endDate": "2014-01-01"
+    }
+  ],
+  "awards": [
+    {
+      "title": "Planned News/Events (Large Newsroom), Winner: Electionland",
+      "date": "2017",
+      "awarder": "Online Journalism Award",
+      "summary": "Collaborative project for U.S. election media monitoring using Check"
+    },
+    {
+      "title": "Planned News/Events (Small Newsroom), Winner: CrossCheck",
+      "date": "2017",
+      "awarder": "Online Journalism Award",
+      "summary": "Collaborative project for French election media monitoring using Check"
+    },
+    {
+      "title": "Winner 2017 - Check",
+      "date": "2017",
+      "awarder": "Innovate Africa Innovation Challenge",
+      "summary": " "
+    },
+    {
+      "title": "Checkdesk",
+      "date": "2014",
+      "awarder": "Knight News Challenge (Prototype)",
+      "summary": " "
+    },
+    {
+      "title": "Checkdesk",
+      "date": "2012",
+      "awarder": "International Press Institute News Innovation Challenge",
+      "summary": " "
+    }
+  ]
+}
+`
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
 import { font, color, Title, PostPreview, Tag, Card, GridItem, DateStamp } from '../styles/shared'
+import { resumeData } from '../data/resume.js'
 
 const ResumePage = styled.div`
   color: black;
@@ -23,15 +24,15 @@ const Grid = styled.div`
     '03 03 03 03'
     '04 04 04 04'
     'X2 X2 X3 X4'
-    'B2 B2 C1 D1'
-    'B3 B3 C2 D1'
-    'B4 B4 C2 D1'
-    'B5 B5 X5 X5'
-    'B6 B6 E1 E1'
-    'B7 B7 11 11'
-    'B8 B8 55 55'
-    'B9 B9 33 33'
-    'B9 B9 22 22';
+    'B0 B0 C1 D1'
+    'B1 B1 C2 D1'
+    'B2 B2 C2 D1'
+    'B3 B3 X5 X5'
+    'B4 B4 E1 E1'
+    'B5 B5 11 11'
+    'B6 B6 55 55'
+    'B7 B7 33 33'
+    'B8 B8 22 22';
 `
 const TitleResume = styled.div`
   color: black;
@@ -59,86 +60,38 @@ const SubHeader = styled.div`
 const Description = styled.div`
   font: ${font.caption};
 `
+const parsedResumeData = JSON.parse(resumeData)
+
 export default function Resume({ data }) {
   return (
     <ResumePage>
       <GridItem area="X1">
         <TitleResume>
-          <p>Chris Gerald Blow</p>
-          <p>Oakland, California 94611</p>
+          <h1>{parsedResumeData.basics.name}</h1>
+          <p>
+            {parsedResumeData.basics.location.city}, {parsedResumeData.basics.location.postalCode}
+          </p>
         </TitleResume>
       </GridItem>
       <Grid>
         <GridItem area="A1">
-          <p>
-            Hello, I’m Chris, a designer living in the San Francisco Bay Area. I work for Meedan, where I've been practicing design strategy for 10
-            years. I have experience in product design, user experience research, front-end programming and investigative journalism. My work
-            typically involves themes of collaborative work and cross-language communication. I am especially interested in user research for the
-            advancement improvement of journalism and civil society.
-          </p>
+          <p>{parsedResumeData.basics.summary}</p>
         </GridItem>
 
         <GridItem area="X2">
           <SectionHeader>Work experience</SectionHeader>
         </GridItem>
-        <GridItem area="B2">
-          <Header>Meedan</Header>
-          <DateStamp>March 2008 - Current</DateStamp>
-          <SubHeader>Director of User Experience</SubHeader>
-          <Description>
-            At Meedan I am a user experience researcher, front-end programmer and a product designer. I support a design team creating social networks
-            and new media projects.
-          </Description>
-        </GridItem>
-        <GridItem area="B3">
-          <Header>Bolt | Peters User Experience</Header>
-          <DateStamp>July 2007 – September 2008</DateStamp>
-          <SubHeader>Researcher, Developer & Designer</SubHeader>
-          <Description>
-            As a User Experience Researcher was responsible for designing studies, analyzing results, collaborating with clients. I created videos,
-            wireframes and sketches. I also helped design and build Ethnio, a Rails app for the user experience industry.
-          </Description>
-        </GridItem>
-        <GridItem area="B4">
-          <Header>Blast Internet Services and Web Design</Header>
-          <DateStamp>March 2006 – July 2007</DateStamp>
-          <SubHeader>Director of Operations & Sales</SubHeader>
-          <Description>
-            I revitalized their web consulting business with user-centric voice. I designed contracts and won budgets to implement an Agile
-            development methodology and a modern Rails stack.
-          </Description>
-        </GridItem>
-        <GridItem area="B5">
-          <Header>Evaluation, Assessment and Policy Connections (EvAP)</Header>
-          <p>UNC-Chapel Hill</p>
-          <DateStamp>2004 – 2006</DateStamp>
-          <SubHeader>Program Assistant & Web Developer</SubHeader>
-          <Description>
-            At EvAP I developed applications for managing stakeholder data, creating educational surveys and gatherinDescription real-time feedback.
-          </Description>
-        </GridItem>
-        <GridItem area="B6">
-          <Header>Nonprofit Design </Header>
-          <DateStamp>2003 – 2006</DateStamp>
-          <SubHeader>Founder and Lead Developer</SubHeader>
-          <Description>
-            I operated a consultancy with clients such as Duke University and the University of North Carolina at Chapel HilDescription.
-          </Description>
-        </GridItem>
-        <GridItem area="B7">
-          <Header>The North Carolina Center on Actual Innocence, Duke and UNC-Chapel Hill</Header>
-          <DateStamp>2003 – 2004</DateStamp>
-          <SubHeader>Teaching Assistant</SubHeader>
-          <Description>
-            I was a teaching assistant for investigative journalism course at the UNC-CH School of Journalism, in partnership with Duke University.
-          </Description>
-        </GridItem>
-        <GridItem area="B8">
-          <Header>The North Carolina Law Review</Header>
-          <DateStamp>2001 – 2003</DateStamp>
-          <SubHeader>Website and Database Developer</SubHeader>
-          <Description>I helped create a custom database for the Law Review, and I managed their website.</Description>
-        </GridItem>
+
+        {parsedResumeData.work.map((job, index) => (
+          <GridItem area={`B${index}`}>
+            <Header>{job.company}</Header>
+            <DateStamp>
+              {job.startDate} - {job.endDate}
+            </DateStamp>
+            <SubHeader>{job.position}</SubHeader>
+            <Description>{job.summary}</Description>
+          </GridItem>
+        ))}
 
         <GridItem area="X3">
           <SectionHeader>Education</SectionHeader>
@@ -171,9 +124,9 @@ export default function Resume({ data }) {
         </GridItem>
         <GridItem area="E1">
           <p>Online Journalism Award, Planned News/Events (Large Newsroom), Winner 2017 - Electionland</p>
-          <p>Online Journalism Award, Planned News/Events (Small Newsroom), Winner 2017 - CrossCheck</p>
-          <p>Innovate Africa Innovation Challenge, Winner 2017 - Check</p>
-          <p>Knight News Challenge (Prototype), Winner 2014 - Checkdesk</p>
+          <p>Online Journalism Award, </p>
+          <p />
+          <p />
           <p>International Press Institute News Innovation Challenge, Winner 2012 - Checkdesk</p>
           <p>Africa News Innovation Challenge, Finalist 2012 - Checkdesk</p>
         </GridItem>
