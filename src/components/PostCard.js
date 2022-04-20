55ed09d85e86dcfdc5921b2469313039cbdc18df
Christopher France <christopher@weavegrid.com>
2022-04-19 23:07:22 -0700

    WIP update all dependencies

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
-import Img from 'gatsby-image'
+import Img from 'gatsby-plugin-image'
 import theme from 'theme'
 
 const Card = styled.article`
